79490b78729de61af372c3295937e5149f955d68
diegotestereo <diegotestereo@gmail.com>
2014-07-07 16:20:35 -0300

    etb
    comaensando la programacion del libro...

--- src/com/mdqandroid/controlplc/MainActivity.java
+++ src/com/mdqandroid/controlplc/MainActivity.java
@@ -1,36 +1,90 @@
 package com.mdqandroid.controlplc;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
+
 
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private BluetoothAdapter AdaptadorBT;
+	private String serviceNAme =
+	
+	
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+     
+   }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+   public  void onStart() {
+   super.onStart();
+   ConfigBT();
+   }
 
+
+private void ConfigBT() {
+	AdaptadorBT =BluetoothAdapter.getDefaultAdapter();
+	
+	if(AdaptadorBT==null){
+		Toast.makeText(getApplicationContext(), "El Dispositivo no soporta Bluetooth", Toast.LENGTH_SHORT).show();
+	}else{
+		if(!AdaptadorBT.isEnabled()){
+			Toast.makeText(getApplicationContext(), "Por favor encienda el bluetooth !!!", Toast.LENGTH_SHORT).show();
+					
+		}
+		
+		
+		
+	}
+	
+}
+
+/*
+final Handler mHandler = new Handler() {
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    public void handleMessage(Message msg) {
+            	
+                switch (msg.what) {
+     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   
+                case Mensaje_Escrito:
+                	  byte[] writeBuf = (byte[]) msg.obj;//buffer de escritura...
+                      // Construye un String del Buffer
+                      String writeMessage = new String(writeBuf);
+                      if(D) Log.e(TAG, "Message_write  =w= "+ writeMessage);  
+                    break;
+      //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>                                
+                case Mensaje_Leido:   	                 
+                	byte[] readBuf = (byte[]) msg.obj;//buffer de lectura...
+                    //Construye un String de los bytes validos en el buffer
+                  String readMessage = new String(readBuf, 0, msg.arg1);
+                  if(D) Log.e(TAG, "Message_read   =w= "+ readMessage);              	                 
+                    break;
+     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>	           
+                case Mensaje_Nombre_Dispositivo:
+                    mConnectedDeviceName = msg.getData().getString(DEVICE_NAME); //Guardamos nombre del dispositivo
+     Toast.makeText(getApplicationContext(), "Conectado con "+ mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+     seleccionador=true;
+                    break;
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>                  
+                case Mensaje_TOAST:
+                    Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
+                    Toast.LENGTH_SHORT).show();
+                    break;
+     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+                case MESSAGE_Desconectado:	
+                  	 if(D) Log.e("Conexion","DESConectados");
+                  	 seleccionador=false;             	
+          break;
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  
+                }//FIN DE SWITCH CASE PRIMARIO DEL HANDLER
+            }//FIN DE METODO INTERNO handleMessage
+        };//Fin de Handler*/
+
 }
